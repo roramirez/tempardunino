f2afe49c63a036835104f7efe5e0da2fbd11f0a9
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2015-11-16 19:17:40 -0300

    fixed typo and removed white spaces

--- temp_webserver/temp_webserver.ino
+++ temp_webserver/temp_webserver.ino
@@ -7,7 +7,7 @@ dht11 DHT11;
 
 
 // Set MAC and IP Address
-byte mac[] = { 
+byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192,168,5,177);
 
@@ -17,38 +17,38 @@ EthernetServer server(80);
 void setup(){
 
   Serial.begin(9600);
-  
-   // Init ethernet conection 
+
+   // Init ethernet conection
   Ethernet.begin(mac, ip);
-  
+
   //Start server
   server.begin();
-  
+
   //Log serial
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
-   
-  
+
+
 }
 
 void loop(){
-  
-  
-  // listen 
+
+
+  // listen
   EthernetClient client = server.available();
-  
+
 
   if (client) {
     //Log serial
     Serial.println("new conection");
-    
+
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
-        
+
 
         if (c == '\n' && currentLineIsBlank) {
           //reply for client
@@ -56,24 +56,24 @@ void loop(){
           client.println("Content-Type: application/json");
           client.println("Connnection: close");
           client.println();
-          
-          //set pin sensor 
-          int pin = 2; 
+
+          //set pin sensor
+          int pin = 2;
             // Sensor read
             int chk = DHT11.read(pin);
             switch (chk)
             {
-               case 0: 
+               case 0:
                  client.print("{ \"humidity\": ");
                  client.print((float)DHT11.humidity, DEC);
                  client.print(", \"temperature\": ");
-                 client.print((float)DHT11.temperature, DEC);        
+                 client.print((float)DHT11.temperature, DEC);
                  client.print("}");
                  break;
               case -1:
                  client.print("{error: Checksum error}");
                  break;
-              case -2: 
+              case -2:
                  client.print("{error: Time out error}");
                  break;
               default:
@@ -87,7 +87,7 @@ void loop(){
         if (c == '\n') {
           // you're starting a new line
           currentLineIsBlank = true;
-        } 
+        }
         else if (c != '\r') {
           // you've gotten a character on the current line
           currentLineIsBlank = false;
@@ -98,8 +98,8 @@ void loop(){
     delay(1);
     // close the connection:
     client.stop();
-    Serial.println("client disonnected");
-  
+    Serial.println("client disconnected");
+
   }
-  
+
 }
